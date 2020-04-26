77b39e75683003f1c516c177f5787310e2a2450f
Paul Copplestone <pcopplestone@gmail.com>
2020-04-26 12:57:36 +0800

    Fix the link color

--- build.js
+++ build.js
@@ -229,8 +229,8 @@ const template = (content, currentDate) => {
             <div class="footer" style="clear: both; Margin-top: 10px; text-align: center; width: 100%;">
               <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
                 <tr>
-                    <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;"> 
-                        <a href="${SUBSCRIBE_LINK}" target="_blank" style="display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;">
+                    <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: ${LINK_COLOR}; border-radius: 5px; text-align: center;"> 
+                        <a href="${SUBSCRIBE_LINK}" target="_blank" style="display: inline-block; color: #ffffff; background-color: ${LINK_COLOR}; border: solid 1px ${LINK_COLOR}; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: ${LINK_COLOR};">
                             Like this? Subscribe → 
                         </a> 
                     </td>
